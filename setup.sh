49e1b27d21b6328e12a8d22745d3316ad6530a07
MR-D E R K <107317634+MR-DARK54@users.noreply.github.com>
2022-08-29 03:13:29 +0200

    Add files via upload

---
+++ setup.sh
@@ -0,0 +1,37 @@
+#created by Mr dark
+#https://t.me/name_dark
+
+clear
+echo -e "\033[1;92m  
+https://t.me/name_dark
+\033[1;91m<═══\033[1;41m\033[1;97m Created by D A R K \033[;0m\033[1;91m═══>\033[1;92m"
+
+echo -e '\033[1;92m'
+termux-setup-storage
+echo -e '\033[1;92m'
+apt-get install tor -y
+echo -e '\033[1;92m'
+apt-get install python -y
+echo -e '\033[1;92m'
+#apt-get install python2 -y
+apt-get install wget -y
+echo -e '\033[1;92m'
+pip install lolcat
+echo -e '\033[1;92m'
+pip install --upgrade pip
+echo -e '\033[1;92m'
+pip install requests --upgrade
+echo -e '\033[1;92m'
+pip install requests[socks]
+echo -e '\033[1;92m'
+pip install stem
+echo -e '\033[1;92m'
+pip install instagram-py
+cd $HOME
+echo -e '\033[1;92m'
+wget -O ~/instapy-config.json "https://git.io/v5DGy"
+echo -e '\033[1;92m'
+echo 'Setup is completed \nBruteX is launching...'
+cd $HOME
+rm setup.sh
+python brutex.sh
